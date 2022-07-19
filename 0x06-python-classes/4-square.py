f4e4a11b06e86c98b4875f09242308bc50360859
sakwah <msakwahm@gmail.com>
2022-07-19 23:49:41 +0000

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""this module defines a square and initializes size as positive int
+"""
+
+
+class Square:
+    """square implementation
+    """
+    def __init__(self, size=0):
+        """Initializer
+        Args:
+            size: square size. Defaults to 0
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """area of current square
+        Returns:
+            int: area of the square
+        """
+        return (self.__size ** 2)
