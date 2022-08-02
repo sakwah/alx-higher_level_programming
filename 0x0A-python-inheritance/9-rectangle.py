a6ee0a5cbb4c59b537a46f348151cc42b560424f
sakwah <msakwahm@gmail.com>
2022-08-02 13:23:51 +0000

    class documentation

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -8,6 +8,8 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """A rectangle class."""
+
     def __init__(self, width, height):
         """initialisation
         Args:
